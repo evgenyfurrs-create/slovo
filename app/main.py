544e7c2bda7fb079ae17bcc22f424adea1c93dea
meteor <evgenyfurrs@gmail.com>
2025-09-13 23:29:37 +0300

    First commit

--- app/main.py
+++ app/main.py
@@ -20,6 +20,7 @@ ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 @asynccontextmanager
